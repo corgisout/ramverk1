f6654365b893e73cf68f5688c048e9bf4e5eb8dc
corgisout <simhed98@gmail.com>
2020-01-12 11:37:26 +0000

    kmom 04

--- config/apiKey.php
+++ config/apiKey.php
@@ -6,6 +6,3 @@ return [
     "locationiq" => "2929d73d14f8b3",
 
 ];
-//https://eu1.locationiq.com/v1/search.php?key=2929d73d14f8b3&q=boston&format=json
-//2929d73d14f8b3 lociq
-//3093da73f86f6049a525e5b86e637bdd
